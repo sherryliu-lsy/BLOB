e00d0653d8f00244c41ab783d9432accdf4e5b7b
Sherry Liu <liushuyu.cn@gmail.com>
2024-09-27 00:06:29 -0400

    cost functions example: MAE, MSE, RMSE, LRMSE

---
+++ ex_cost_functions.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+cgpa = [6.89, 5.12, 7.82, 7.42, 6.94, 7.89, 6.73, 6.75, 6.09]
+package = [3.26, 1.98, 3.25, 3.67, 3.57, 2.99, 2.6, 2.48, 2.31]
+df = pd.DataFrame({'cgpa' : cgpa, 'package' : package})
+y = df['package']
+X = df.drop('package', axis = 1)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=2)
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+y_pred = lr.predict(X_test)
+print(y_pred)
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
+print("MAE",mean_absolute_error(y_test,y_pred))
+print("MSE",(mean_squared_error(y_test,y_pred)))
+print("RMSE",np.sqrt(mean_squared_error(y_test,y_pred)))
+print("LRMSE",np.log(np.sqrt(mean_squared_error(y_test,y_pred))))
