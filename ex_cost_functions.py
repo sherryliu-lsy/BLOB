a9b307810804ec672f0b524ea9e1dacc74c6144c
Sherry Liu <liushuyu.cn@gmail.com>
2024-09-27 00:06:55 -0400

    some formatting

--- ex_cost_functions.py
+++ ex_cost_functions.py
@@ -1,5 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 import pandas as pd
 
 cgpa = [6.89, 5.12, 7.82, 7.42, 6.94, 7.89, 6.73, 6.75, 6.09]
@@ -15,8 +17,6 @@ lr.fit(X_train,y_train)
 y_pred = lr.predict(X_test)
 print(y_pred)
 
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-import numpy as np
 print("MAE",mean_absolute_error(y_test,y_pred))
 print("MSE",(mean_squared_error(y_test,y_pred)))
 print("RMSE",np.sqrt(mean_squared_error(y_test,y_pred)))
